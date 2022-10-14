8b7cf22ac03e8b607809b636ef18bf14c573c036
BlackCarDriver <13169346002@163.com>
2022-10-14 19:08:56 +0800

    temp commit

--- common/util/util_test.go
+++ common/util/util_test.go
@@ -1,12 +1,7 @@
 package util
 
 import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
-	"net/http"
 	"testing"
-	"time"
 )
 
 // http://push2his.eastmoney.com/api/qt/stock/trends2/get?fields1=f1,f2,f3,f4,f5,f6,f7,f8,f9,f10,f11,f12,f13&
@@ -29,15 +24,6 @@ type kLineParams struct {
 	Timestamp int64  `json:"_"`
 }
 
-type Params2 struct {
-	Fields    string `json:"fields"`
-	CB        string `json:"cb"`
-	Fltt      int    `json:"fltt"`
-	SECIDS    string `json:"secids"` // 选中的股票ID, 参考: "1.000001,0.399001"
-	UT        string `json:"ut"`
-	Timestamp int64  `json:"_"`
-}
-
 // https://push2.eastmoney.com/api/qt/ulist.np/get?
 // cb=jQuery112305582875234802821_1665462176107&
 // fltt=2&
@@ -45,55 +31,6 @@ type Params2 struct {
 // fields=f1,f2,f3,f4,f6,f12,f13,f104,f105,f106&
 // ut=b2884a393a59ad64002292a3e90d46a5&
 // _=1665462176108
-
-type UtilRespLayer1 struct {
-	Data UtilRespLayer2 `json:"data"`
-}
-
-type UtilRespLayer2 struct {
-	Total int            `json:"total"`
-	Diff  []UtilRespData `json:"diff"`
-}
-
-type UtilRespData struct {
-	F2   float64 `json:"f2"`   // 现价
-	F3   float64 `json:"f3"`   // 增幅
-	F4   float64 `json:"f4"`   // 增值
-	F6   float64 `json:"f6"`   // 总市值
-	F12  string  `json:"f12"`  // 股票代码
-	F104 int     `json:"f104"` // 涨_数量
-	F105 int     `json:"f105"` // 跌_数量
-	F106 int     `json:"f106"` // 平_数量
-}
-
-func GetSummaryInfo() {
-	timestamp := time.Now().Unix()
-	params := Params2{
-		Fltt:      2,
-		SECIDS:    "1.000001,0.399001",
-		UT:        "b2884a393a59ad64002292a3e90d46a5",
-		Fields:    "f1,f2,f3,f4,f6,f12,f13,f104,f105,f106",
-		Timestamp: timestamp,
-	}
-	resp, err := GetRequireWithParams("https://push2.eastmoney.com/api/qt/ulist.np/get", params)
-	if err != nil {
-		fmt.Printf("get data fail: err=%v \n", err)
-		return
-	}
-	if resp.StatusCode != http.StatusOK {
-		fmt.Printf("unexpect status: %d \n", resp.StatusCode)
-		return
-	}
-	data, _ := ioutil.ReadAll(resp.Body)
-	var ret UtilRespLayer1
-	err = json.Unmarshal(data, &ret)
-	if err != nil {
-		fmt.Printf("unmarshal fail: err=%v data=%s \n", err, string(data))
-		return
-	}
-	fmt.Printf("response=%+v", ret.Data)
-}
-
 func TestGetRequireWithParams(t *testing.T) {
-	GetSummaryInfo()
+
 }
